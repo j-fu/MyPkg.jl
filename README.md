bda1f759fedafd45ce4cdd99178bc1523263d209
Jürgen Fuhrmann <juergen-fuhrmann@web.de>
2024-10-24 10:16:16 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.52

---
+++ README.md
@@ -0,0 +1,6 @@
+# MyPkg
+
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://j-fu.github.io/MyPkg.jl/stable/)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://j-fu.github.io/MyPkg.jl/dev/)
+[![Build Status](https://github.com/j-fu/MyPkg.jl/actions/workflows/CI.yml/badge.svg?branch=main)](https://github.com/j-fu/MyPkg.jl/actions/workflows/CI.yml?query=branch%3Amain)
+[![Coverage](https://codecov.io/gh/j-fu/MyPkg.jl/branch/main/graph/badge.svg)](https://codecov.io/gh/j-fu/MyPkg.jl)
---
+++ docs/make.jl
@@ -0,0 +1,23 @@
+using MyPkg
+using Documenter
+
+DocMeta.setdocmeta!(MyPkg, :DocTestSetup, :(using MyPkg); recursive=true)
+
+makedocs(;
+    modules=[MyPkg],
+    authors="Jürgen Fuhrmann <juergen-fuhrmann@web.de> and contributors",
+    sitename="MyPkg.jl",
+    format=Documenter.HTML(;
+        canonical="https://j-fu.github.io/MyPkg.jl",
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
+
+deploydocs(;
+    repo="github.com/j-fu/MyPkg.jl",
+    devbranch="main",
+)
